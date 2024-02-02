e8d0dacc8406046789d9d61064122fcc9b786854
Emababy <ziadembaby10@gmail.com>
2024-02-02 23:44:08 +0200

    updated
    Last

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 // next.config.js
-
 module.exports = {
       // Add other configuration options if needed
-    
-      // Specify the output directory for the export
       distDir: 'out',
     };
     
